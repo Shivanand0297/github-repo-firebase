1b156848379308e1d475c8311540347e07ac3d77
Shivanand <Shivanand0297@gmail.com>
2022-12-31 15:06:09 +0530

    removed config

---
+++ src/config/FirebaseConfig.sample.js
@@ -0,0 +1,12 @@
+// this config will be provided by the firebase
+const firebaseConfig = {
+    apiKey: "your api key",
+    authDomain: "------",
+    projectId: "------",
+    storageBucket: "------",
+    messagingSenderId: "------",
+    appId: "------",
+    measurementId: "------"
+  };
+
+  export default firebaseConfig
